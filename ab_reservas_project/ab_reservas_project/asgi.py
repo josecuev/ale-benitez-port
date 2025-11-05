7d044a90fa283771877a9655ebeb1d825a3745a1
josecuevas <jmcuev89@gmail.com>
2025-11-05 18:36:03 -0300

    Adds initial Django project structure
    Sets up the basic structure for a Django project using Poetry for dependency management and Docker for containerization.
    
    This commit initializes a Django project named "ab_reservas_project" with the necessary files and configurations:
    - Creates a basic Django project structure with settings, URLs, and WSGI configuration.
    - Adds an `app_reservas` Django app for reservation-related functionality.
    - Defines models for managing reservations, including services, schedules, and reservation details.
    - Configures a Docker environment with a Dockerfile to build the application image and a docker-compose file to orchestrate the services, using Python 3.12 and Poetry.

---
+++ ab_reservas_project/ab_reservas_project/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ab_reservas_project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ab_reservas_project.settings')
+
+application = get_asgi_application()
---
+++ ab_reservas_project/app_reservas/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppReservasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_reservas'
