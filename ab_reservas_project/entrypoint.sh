7932181cce5733a93e5eb217f1f8d0eff8742d22
josecuevas <jmcuev89@gmail.com>
2026-02-21 18:34:33 -0300

    entrypoint arreglado

--- ab_reservas_project/entrypoint.sh
+++ ab_reservas_project/entrypoint.sh
@@ -6,4 +6,5 @@ echo "==> migrate..."
 poetry run python manage.py migrate --noinput
 echo "==> Uvicorn..."
 exec poetry run uvicorn ab_reservas_project.asgi:application \
-  --host 0.0.0.0 --port 8000 --workers 2
+  --host 0.0.0.0 --port 8000 --workers 2 \
+  --proxy-headers --forwarded-allow-ips='*'
